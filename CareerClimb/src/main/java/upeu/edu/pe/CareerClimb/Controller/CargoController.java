0497eac72360a1fb8e737e1c7661ae6f2b1c56ee
Assarot <saemnapan@gmail.com>
2024-10-30 15:05:58 -0500

    v2.1

--- CareerClimb/src/main/java/upeu/edu/pe/CareerClimb/Controller/CargoController.java
+++ CareerClimb/src/main/java/upeu/edu/pe/CareerClimb/Controller/CargoController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,8 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import upeu.edu.pe.CareerClimb.Entity.AccesoRol;
 import upeu.edu.pe.CareerClimb.Entity.Cargo;
 import upeu.edu.pe.CareerClimb.Service.CargoService;
 
