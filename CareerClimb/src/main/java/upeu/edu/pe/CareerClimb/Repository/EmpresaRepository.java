6a8de5f8fd92326585f0fa85a871430a3af3659c
Marquitoz <markst1811@gmail.com>
2024-11-07 18:12:42 -0500

    Procedure delete casacade impl

--- CareerClimb/src/main/java/upeu/edu/pe/CareerClimb/Repository/EmpresaRepository.java
+++ CareerClimb/src/main/java/upeu/edu/pe/CareerClimb/Repository/EmpresaRepository.java
@@ -2,7 +2,7 @@ package upeu.edu.pe.CareerClimb.Repository;
 
 import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import upeu.edu.pe.CareerClimb.Entity.Empresa;
@@ -10,6 +10,6 @@ import upeu.edu.pe.CareerClimb.Entity.Empresa;
 public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
 	@Modifying
 	@Transactional
-	@Query(value = "CALL sp_delete_empresa_by_ruc(:empresaRuc)", nativeQuery = true)
+	@Procedure(procedureName = "sp_delete_empresa_by_ruc")
 	void deleteEmpresa(@Param("empresaRuc") String empresaRuc);
 }
